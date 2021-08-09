f361edb03ca4a6983cdbd8b55ee0a61317e35946
edsoncarlosdvp <edsoncarlos@ymail.com>
2021-08-09 01:25:56 -0300

    NextJS aula 05 => Link. Navegando entre páginas#02.

--- next-basilar/src/pages/router/index.tsx
+++ next-basilar/src/pages/router/index.tsx
@@ -1,6 +1,22 @@
 import Link from 'next/link'
+import router, { useRouter } from 'next/router'
 
-export default function router(): JSX.Element {
+export default function rotas(): JSX.Element {
+
+    function simpleNavigation(url: string) {
+        router.push(url)
+    }
+
+    function navigationWithParams() {
+        router.push({
+            pathname: '/router/params',
+            query: {
+                id: 123,
+                name: 'Ana'
+            }
+        })
+    }
+
     return (
         <div>
             <h1>Rota</h1>
@@ -21,6 +37,11 @@ export default function router(): JSX.Element {
                     </a>
                 </Link>
             </ul>
+            <div style={{ display: 'flex' }}>
+                <button onClick={navigationWithParams}>Params</button>
+                <button onClick={() => router.push('/router/123/search')}>Buscar</button>
+                <button onClick={() => simpleNavigation('/router/456/Carlos')}>Carlos</button>
+            </div>
         </div>
     )
 }
