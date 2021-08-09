558ae0f06c91b076ad8640dd8498a3e660baeff9
edsoncarlosdvp <edsoncarlos@ymail.com>
2021-08-08 21:33:07 -0300

    NextJS aula 01 => Router#01

---
+++ next-basilar/src/pages/_otherRouter.tsx
@@ -0,0 +1,7 @@
+export default function _otherRouter(): JSX.Element {
+    return (
+        <div>
+            <h1>_Outra Rota</h1>
+        </div>
+    )
+}
---
+++ next-basilar/src/pages/router/index.tsx
@@ -0,0 +1,7 @@
+export default function router(): JSX.Element {
+    return (
+        <div>
+            <h1>Rota</h1>
+        </div>
+    )
+}
