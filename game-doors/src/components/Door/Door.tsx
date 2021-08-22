b4ede12d92022342bfa02b5a1b6c726029042d2a
edsoncarlosdvp <edsoncarlos@ymail.com>
2021-08-22 16:20:13 -0300

    Add events in the door #01

--- game-doors/src/components/Door/Door.tsx
+++ game-doors/src/components/Door/Door.tsx
@@ -2,17 +2,17 @@ import DoorModel from '../../model/DoorModel'
 import { Container, Frame, Content, NumberDoor, Knob, Selected, Floor } from './DoorStyles'
 
 interface DoorProps {
-    door: DoorModel
+    value: DoorModel
 }
 
 export const Door = (props: DoorProps): JSX.Element => {
-    const { door } = props
+    const value = props.value
     const selected = props.Selected ? props.Selected : ''
     return (
         <Container>
             <Frame className={`${selected}`}>
                 <Content>
-                    <NumberDoor>{door.num}</NumberDoor>
+                    <NumberDoor>{value.num}</NumberDoor>
                     <Knob />
                 </Content>
             </Frame>
--- game-doors/src/pages/index.tsx
+++ game-doors/src/pages/index.tsx
@@ -1,16 +1,13 @@
+import { useState } from 'react'
 import { Door } from '../components/Door'
 import DoorModel from '../model/DoorModel'
 import { Container } from '../styles/home'
 
 export default function Home(): JSX.Element {
-    const p1 = new DoorModel(1)
-    const p2 = new DoorModel(2)
-    const p3 = new DoorModel(3)
+    const [p1, setP1] = useState(new DoorModel(1))
     return (
         <Container>
-            <Door door={p1} />
-            <Door door={p2} />
-            <Door door={p3} />
+            <Door value={p1} />
         </Container>
     )
 }
