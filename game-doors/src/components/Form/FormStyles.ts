cc2699342b976b09cd19b32ecf3c94710cf6a7d5
edsoncarlosdvp <edsoncarlos@ymail.com>
2021-09-04 23:35:06 -0300

    Adjust

--- game-doors/src/components/Form/FormStyles.ts
+++ game-doors/src/components/Form/FormStyles.ts
@@ -1,8 +1,20 @@
+import link from 'next/link'
 import styled from 'styled-components'
 
-export const ContainerForm = styled.div`
+const Container = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
 `
+
+const Content = styled.div`
+    display: flex;
+`
+
+const LinkStyles = styled.a`
+    cursor: pointer;
+`
+
+export { Container, Content, LinkStyles}
--- game-doors/src/pages/index.tsx
+++ game-doors/src/pages/index.tsx
@@ -1,22 +1,24 @@
-import Link from 'next/link'
+//import Link from 'next/link'
 import { Card } from '@components/Card/Card'
-import { ContainerForm } from '@components/Form'
+import { Container, Content, LinkStyles } from '@components/Form'
 
 export default function Form(): JSX.Element {
     return (
-        <ContainerForm>
-            <div>
+        <Container>
+            <Content>
                 <Card bgcolor="#c0392c"><h1>Monty Hall</h1></Card>
                 <Card />
-            </div>
-            <div>
+            </Content>
+            <Content>
                 <Card />
                 <Card bgcolor="#28a085">
-                    <Link href="./game/10/2">
-                        <h1>Iniciar</h1>
-                    </Link>
+                    <LinkStyles>
+                        <a href="./game/10/2">
+                            <h1>Iniciar</h1>
+                        </a>
+                    </LinkStyles>
                 </Card>
-            </div>
-        </ContainerForm>
+            </Content>
+        </Container>
     )
 }
